6ab069c49b6184866a361bcc7c83cf7713479ae0
pmthahlong001-arch <pmthahlong001@gmail.com>
2026-06-09 06:20:15 +0700

    Create oloRaceCommand.java

---
+++ src/main/java/online/racesmp/holorace/commands/oloRaceCommand.java
@@ -0,0 +1,46 @@
+package online.racesmp.holorace.commands;
+
+import online.racesmp.holorace.HoloRace;
+import online.racesmp.holorace.utils.MessageUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HoloRaceCommand implements CommandExecutor {
+
+    private final HoloRace plugin;
+
+    public HoloRaceCommand(HoloRace plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("holorace.admin")) {
+                sender.sendMessage("§cBạn không có quyền thực hiện lệnh này!");
+                return true;
+            }
+
+            // Thực hiện reload cấu hình và data
+            plugin.reloadConfig();
+            plugin.getRaceManager().loadRaces();
+            plugin.getPlayerDataManager().loadData();
+            MessageUtil.reload(plugin);
+
+            sender.sendMessage("§a[HoloRace] Đã reload toàn bộ cấu hình hệ thống thành công!");
+            return true;
+        }
+
+        // Lệnh mở GUI chủng tộc cho người chơi
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            // Gọi hàm mở menu từ GUIManager của bạn
+            plugin.getGUIManager().openMenu(player); 
+        } else {
+            sender.sendMessage("§cKhung lệnh này chỉ có thể thực hiện bởi người chơi trong game!");
+        }
+        return true;
+    }
+}
